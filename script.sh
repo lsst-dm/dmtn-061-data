5d35a25fd53d41809136fa192268c70863ab8aac
David Reiss <reiss@uw.edu>
2017-09-21 17:28:19 -0700

    Add scripts and configs

---
+++ script.sh
@@ -0,0 +1,83 @@
+rm -r decamDirTestNEW
+imageDifference2.py calexpDir_b1631 --output decamDirTestNEW \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimconfig.py --clobber-config --clobber-versions >& output_NEW.txt
+
+imageDifference.py calexpDir_b1631 --output decamDirTestOLD \
+                   --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                   --configfile diffimconfig_OLD.py --clobber-config --clobber-versions >& output_OLD.txt
+
+
+/bin/cp -fv txt output_NEW.txt decamDirTestNEW/config/deepDiff.py /tmp/
+rpl -q makeDiffim. '' /tmp/output_NEW.txt /tmp/deepDiff.py
+rpl -q .makeDiffim '' /tmp/output_NEW.txt /tmp/deepDiff.py
+rpl -q processDiffim. '' /tmp/output_NEW.txt /tmp/deepDiff.py
+rpl -q .processDiffim '' /tmp/output_NEW.txt /tmp/deepDiff.py
+diff /tmp/output_NEW.txt output_OLD.txt
+diff /tmp/deepDiff.py decamDirTestOLD/config/deepDiff.py
+
+exit
+
+# Metrics dont work (for both versions!)
+imageDifference2.py calexpDir_b1631 --output decamDirTestNEW \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimconfig.py --clobber-config --clobber-versions \
+                    --config makeDiffim.doAddMetrics=True
+
+imageDifference.py calexpDir_b1631 --output decamDirTestOLD \
+                   --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                   --configfile diffimconfig_OLD.py --clobber-config --clobber-versions \
+                   --config doAddMetrics=True
+
+
+# Try Zogy
+imageDifference2.py calexpDir_b1631 --output decamDirTestNEWZogy \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile diffimconfig.py --clobber-config --clobber-versions \
+                    --config makeDiffim.subtract='zogy' >& output_NEW_ZOGY.txt
+
+imageDifference.py calexpDir_b1631 --output decamDirTestOLDZogy \
+                   --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                   --configfile diffimconfig_OLD.py --clobber-config --clobber-versions \
+                   --config subtract='zogy' >& output_OLD_ZOGY.txt
+
+/bin/cp -fv txt output_NEW_ZOGY.txt decamDirTestNEWZogy/config/deepDiff.py /tmp/
+rpl -q makeDiffim. '' /tmp/output_NEW_ZOGY.txt /tmp/deepDiff.py
+rpl -q .makeDiffim '' /tmp/output_NEW_ZOGY.txt /tmp/deepDiff.py
+rpl -q processDiffim. '' /tmp/output_NEW_ZOGY.txt /tmp/deepDiff.py
+rpl -q .processDiffim '' /tmp/output_NEW_ZOGY.txt /tmp/deepDiff.py
+diff /tmp/output_NEW_ZOGY.txt output_OLD_ZOGY.txt
+
+exit
+
+
+# Try individual new command-line tasks. Not sure how they'll work.
+
+makeDiffim.py calexpDir_b1631 --output decamDirTest2 \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile makeDiffimConfig.py --clobber-config --clobber-versions \
+                    --config subtract='al'
+
+processDiffim.py decamDirTest2 --output decamDirTest2a \
+                    --id visit=289820 ccdnum=11 --templateId visit=288976 \
+                    --configfile processDiffimConfig.py --clobber-config --clobber-versions
+
+ls -al decamDirTest2a/deepDiff/v289820/diaSrc-11.fits decamDirTestOLD/deepDiff/v289820//diaSrc-11.fits
+
+
+
+# Test with `doUseRegister=True` -- seems to work now that I fixed it
+imageDifference2.py calexpDir_b1631 --output decamDirTest_Zogy_noSpatial_doRegister --id visit=289820 ccdnum=11 --templateId visit=288976 --configfile diffimconfig.py --clobber-config --clobber-versions --config makeDiffim.subtract='zogy' --config makeDiffim.doUseRegister=True
+
+
+# Test Zogy making Scorr image (param doPreConvolve=True)
+imageDifference2.py calexpDir_b1631 --output decamDirTest_ZogyScorr_noSpatial --id visit=289820 ccdnum=11 --templateId visit=288976 --configfile diffimconfig.py --clobber-config --clobber-versions --config makeDiffim.subtract='zogy' --config makeDiffim.doPreConvolve=True
+
+
+# Test decorrelation + preconvolution
+# Need to switch the template and visit Id here...
+imageDifference2.py calexpDir_b1631 --output decamDirTest_ALDpreconv --id visit=288976 ccdnum=11 --templateId visit=289820 --configfile diffimconfig.py --clobber-config --clobber-versions --config makeDiffim.subtract='al' --config makeDiffim.doPreConvolve=True --config makeDiffim.doDecorrelation=False
+imageDifference2.py calexpDir_b1631 --output decamDirTest_ALDecorPreconv --id visit=288976 ccdnum=11 --templateId visit=289820 --configfile diffimconfig.py --clobber-config --clobber-versions --config makeDiffim.subtract='al' --config makeDiffim.doPreConvolve=True --config makeDiffim.doDecorrelation=True
+
+# Huh, there's an offset in the diffim with preconv=true (even with decorr=false). Test in the original version.
+imageDifference.py calexpDir_b1631 --output decamDirTest_ALpreconv_OLD --id visit=288976 ccdnum=11 --templateId visit=289820 --configfile diffimconfig_OLD.py  --clobber-config --clobber-versions --config subtract='al' --config doPreConvolve=True --config doDecorrelation=False
